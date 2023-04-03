c01174c86aecd8fb68c999a47ba8ef2aecf6a110
ib1zza <dremast1337@gmail.com>
2023-04-03 19:55:34 +0300

    added timestamp to every message and fixed AppRouter wrong redirect

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -42,7 +42,6 @@ const AppRouter: React.FC<{ isAuth: boolean }> = ({ isAuth }) => {
           {publicRoutes.map(({ path, element }) => (
             <Route key={path} path={path} element={element} />
           ))}
-          <Route path={"*"} element={<Navigate to={AppRoute.Login} />} />
         </>
       )}
     </Routes>
