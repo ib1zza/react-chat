ea4ffdd6808d6edcbc4ae7aa527dd897095c34bd
ib1zza <dremast1337@gmail.com>
2023-04-30 08:16:46 +0300

    pwa first try

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const vitePWA = VitePWA({
+  registerType: "autoUpdate",
+  outDir: "dist",
+  manifest: {
+    name: "React Chat",
+    short_name: "RChat",
+    description: "A simple chat app",
+    theme_color: "#3e3c61",
+    icons: [
+      {
+        src: "assets/images/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
@@ -11,4 +29,6 @@ export default defineConfig({
       assets: "/src/assets",
     },
   },
+
+  plugins: [react(), vitePWA],
 });
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+export default {
+  globDirectory: "build/",
+  globPatterns: [
+    "**/*.{js,jsx,ts,tsx,css, woff2, png, jpg, jpeg, svg,md,json}",
+  ],
+  swDest: "build/sw.js",
+};
